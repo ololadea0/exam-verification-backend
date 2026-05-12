edffcdcfcfdcda5bee57b37ef0eda86722c8cd34
ololadea0 <opeyemifawaz11@gmail.com>
2026-05-12 15:17:22 +0100

    trying to fix cors issue

--- server.js
+++ server.js
@@ -37,17 +37,31 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL
+];
+
 app.use(cors({
-    origin: [
-        process.env.FRONTEND_URL || "http://localhost:5173",
-    ],
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin))
+        {
+            callback(null, true);
+        } else
+        {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
 }));
+
+app.use(cookieParser());
+
 app.use(compression());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 
-app.use(cookieParser());
+
 
 app.use('/api/admin', adminRoutes);// Admin routes
 app.use('/api/students', studentRoutes);// Student routes
