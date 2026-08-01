01491105362fdaa396738140f41c3c050a3cbc7d
ololadea0 <opeyemifawaz11@gmail.com>
2026-08-01 13:23:12 +0100

    session issue for iphone

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,22 +5,33 @@ import Admin from "../models/adminModel.js";
 const protect = asyncHandler(async (req, res, next) => {
     const token = req.cookies?.adminToken;
 
+    console.log("=== PROTECT ===");
+    console.log("URL:", req.originalUrl);
+    console.log("Method:", req.method);
+    console.log("Token exists:", !!token);
+
     if (!token)
     {
+        console.log("❌ NO TOKEN");
         return res.status(401).json({
             message: "Not authorized, no session"
         });
     }
 
-
     try
     {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+        console.log("✅ JWT valid");
+        console.log("Decoded ID:", decoded.id);
+
         const admin = await Admin.findById(decoded.id).select("-password");
+
+        console.log("Admin found:", !!admin);
 
         if (!admin)
         {
+            console.log("❌ ADMIN NOT FOUND");
             return res.status(401).json({
                 message: "Not authorized, admin not found"
             });
@@ -31,13 +42,12 @@ const protect = asyncHandler(async (req, res, next) => {
 
     } catch (error)
     {
+        console.log("❌ JWT ERROR:", error.message);
+
         return res.status(401).json({
             message: "Not authorized, session failed"
         });
     }
-    console.log("Origin:", req.headers.origin);
-    console.log("Cookie header:", req.headers.cookie);
-    console.log("Parsed cookies:", req.cookies);
 });
 
 export { protect };
